586bf2cd2fabb06309936b324f95c26a4cbc3b57
mohammedterfa <mohammedterfa@gmail.com>
2022-03-11 11:08:12 +0200

    protected store and update and destroy routes

--- routes/api.php
+++ routes/api.php
@@ -19,14 +19,18 @@ use Illuminate\Support\Facades\Route;
 /* Route::get('/products', [ProductController::class, 'index']);
 
 Route::post('/products',[ProductController::class, 'store']); */
+//Route::resource('products', ProductController::class);
 
 
 //public routes
-Route::resource('products', ProductController::class);
-//Route::get('/products/search/{name}', [ProductController::class, 'search']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
 
 //protocted routes
 Route::group(['middleware' => ['auth:sanctum']], function (){
-    Route::get('/products/search/{name}', [ProductController::class, 'search']);
+    Route::post('/products',[ProductController::class, 'store']);
+    Route::put('/products/{id}',[ProductController::class, 'update']);
+    Route::delete('/products/{id}',[ProductController::class, 'destroy']);
 });
